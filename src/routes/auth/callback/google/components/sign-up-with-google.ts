53f3905b727ad05cc954de9986b6fa97d326821a
davitti <abuadonald@gmail.com>
2025-06-16 00:57:06 +0100

    refactor(auth): link Google OAuth sign-up to guest user and unify redirect paths
    - Modified `signUpWithGoogle` to update existing guest user instead of inserting a new one
    - Used `guestId` from session context to maintain user continuity
    - Replaced all OAuth redirect routes from `/auth/oauth` to `/auth/sign-in` for consistency

--- src/routes/auth/callback/google/components/sign-up-with-google.ts
+++ src/routes/auth/callback/google/components/sign-up-with-google.ts
@@ -2,17 +2,20 @@ import { db } from "@db/connect-db";
 import { profileSchema } from "@db/schema/profile.schema";
 import { userSchema } from "@db/schema/user.schema";
 import { GoogleProfile } from "../google.controller";
+import { eq } from "drizzle-orm";
 
-export async function signUpWithGoogle(profile: GoogleProfile) {
+export async function signUpWithGoogle(userId: string, profile: GoogleProfile) {
 	const [user] = await db
-		.insert(userSchema)
-		.values({
+		.update(userSchema)
+		.set({
 			email: profile.email,
 			emailVerified: profile.email_verified,
 			firstName: profile.given_name,
 			lastName: profile.family_name,
 			picture: profile.picture,
+			id: userId,
 		})
+		.where(eq(userSchema.id, userId))
 		.returning();
 
 	await db.insert(profileSchema).values({
--- src/routes/auth/callback/google/google.controller.ts
+++ src/routes/auth/callback/google/google.controller.ts
@@ -42,22 +42,22 @@ export async function googleController(req: Request, res: Response) {
 			} else
 				res.redirect(
 					302,
-					`${process.env.ORIGIN!}/auth/oauth?status=not-authenticated`,
+					`${process.env.ORIGIN!}/auth/sign-in?status=not-authenticated`,
 				);
 		} else if (user) {
 			if (user.linkOauthAccounts) {
 				req.session.user = user;
 				res.redirect(
 					302,
-					`${process.env.ORIGIN!}/auth/oauth?status=authenticated`,
+					`${process.env.ORIGIN!}/auth/sign-in?status=authenticated`,
 				);
 			} else
 				res.redirect(
 					302,
-					`${process.env.ORIGIN!}/auth/oauth?status=not-linked`,
+					`${process.env.ORIGIN!}/auth/sign-in?status=not-linked`,
 				);
 		} else {
-			const user = await signUpWithGoogle(profile);
+			const user = await signUpWithGoogle(req.session.ctx?.guest?.id!, profile);
 			req.session.user = user;
 			res.redirect(
 				302,
@@ -69,7 +69,7 @@ export async function googleController(req: Request, res: Response) {
 
 		res.redirect(
 			302,
-			`${process.env.ORIGIN!}/auth/oauth?status=not-authenticated`,
+			`${process.env.ORIGIN!}/auth/sign-in?status=not-authenticated`,
 		);
 	}
 }
